f0f034702573b3b300ab3cb69a8aa12ffc0baa42
Gian-Reto Tarnutzer <gian@cloudpages.ch>
2021-01-30 00:45:16 +0100

    Efficiency improvements

--- src/showcase360.js
+++ src/showcase360.js
@@ -1,50 +1,76 @@
 const showcase360 = (el, options) => {
-  const settings = Object.assign({
-    images: [],
-    speed: 10,
-    play: false,
-    auto: true,
-    edgeStop: false,
-  }, options);
+  const settings = Object.assign(
+    {
+      images: [],
+      speed: 10,
+      preload: true,
+      mode: "controlled", // "controlled", "autoplay" or "interactive"
+    },
+    options
+  );
 
   const speed = settings.speed;
   const container = el;
-  const images = settings.images;
-  const image = container.querySelector('img');
+  const imageSources = settings.images;
+
+  let images = [];
   let preloaded = false;
-  let frame = 0;
+  let previousFrame;
+  let currentFrame = 0;
   let timer;
 
   const preload = (index) => {
     index = index || 0;
 
-    if (images && images.length > index) {
-      let img = new Image ();
+    if (imageSources && imageSources.length > index) {
+      let img = new Image();
 
       img.onload = function() {
         preload(index + 1);
-      }
+      };
 
-      img.src = images[index];
+      img.src = imageSources[index];
+
+      resolveSrc(index);
     }
 
     preloaded = true;
   };
 
+  const resolveSrc = (index) => {
+    let image = images[index];
+
+    if (image.dataset.src) {
+      image.src = image.dataset.src;
+      image.removeAttribute("data-src");
+    }
+  };
+
+  const updateFrame = () => {
+    resolveSrc(currentFrame);
+
+    if (previousFrame) {
+      images[previousFrame].style.visibility = "hidden";
+    }
+
+    images[currentFrame].style.visibility = "visible";
+  };
+
   const move = (speed) => {
     timer = setInterval(() => {
-      frame += 1;
+      previousFrame = currentFrame;
+      currentFrame += 1;
 
-      if (frame >= images.length) {
-        frame = 0;
+      if (currentFrame >= imageSources.length) {
+        currentFrame = 0;
       }
 
-      image.setAttribute('src', images[frame]);
+      updateFrame();
 
-      const ev = new CustomEvent('slideChanged', {
+      const ev = new CustomEvent("slideChanged", {
         detail: {
-          currentSlide: frame
-        }
+          currentSlide: currentFrame,
+        },
       });
 
       container.dispatchEvent(ev);
@@ -52,46 +78,81 @@ const showcase360 = (el, options) => {
   };
 
   const play = () => {
-    preload();
     move(speed);
   };
 
   const pause = () => {
     clearInterval(timer);
-    image.setAttribute('src', images[frame]);
+    updateFrame();
   };
 
-  const auto = () => {
-    ['mouseenter', 'touchstart'].forEach((e) => {
-      container.addEventListener(e, (event) => {
-        play();
-      }, false);
+  const startInteractive = () => {
+    ["mouseenter", "touchstart"].forEach((e) => {
+      container.addEventListener(
+        e,
+        (event) => {
+          play();
+        },
+        false
+      );
     });
 
     // Hack iOS (and potential other mobile browsers) image saving ability
-    container.style.webkitTouchCallout = 'none';
-    container.style.webkitUserSelect = 'none';
-    container.addEventListener('contextmenu', (event) => {
+    container.style.webkitTouchCallout = "none";
+    container.style.webkitUserSelect = "none";
+    container.addEventListener("contextmenu", (event) => {
       event.preventDefault();
       event.stopPropagation();
       return false;
     });
 
-    ['mouseleave', 'touchend', 'touchcancel'].forEach((e) => {
-      container.addEventListener(e, (event) => {
-        event.preventDefault();
-        event.stopPropagation();
+    ["mouseleave", "touchend", "touchcancel"].forEach((e) => {
+      container.addEventListener(
+        e,
+        (event) => {
+          event.preventDefault();
+          event.stopPropagation();
 
-        pause();
-      }, false);
+          pause();
+        },
+        false
+      );
     });
   };
 
+  const setupDOMElements = () => {
+    container.style.position = "relative";
+
+    imageSources.forEach((imageSource, index) => {
+      const image = document.createElement("img");
+
+      if (index > 0) {
+        image.dataset.src = imageSource;
+        image.style.visibility = "hidden";
+        image.style.position = "absolute";
+        image.style.top = 0;
+        image.style.left = 0;
+      } else {
+        image.src = imageSource;
+      }
+
+      container.appendChild(image);
+    });
+
+    images = container.querySelectorAll("img");
+  };
+
   const init = () => {
-    if (settings.play === true) {
+    setupDOMElements();
+
+    if (settings.preload) {
+      preload();
+    }
+
+    if (settings.mode == "autoplay") {
       play();
-    } else if (settings.auto === true) {
-      auto();
+    } else if (settings.mode == "interactive") {
+      startInteractive();
     }
   };
 
